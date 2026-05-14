88000b8bc7fc1d2254ac652fbf41b57ffd146c18
agrigorakis-tech <agrigorakis@programize.com>
2026-05-14 18:12:26 +0300

    Create azure.js

---
+++ azure.js
@@ -0,0 +1,51 @@
+(function MITOS_AZDO() {
+    'use strict';
+
+    window.MITOS = window.MITOS || {};
+    window.MITOS.azdo.api = window.MITOS.azdo.api || {};
+
+    function apiRequest({ method, url, data = null, headers = {} }) {
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method,
+                url,
+                headers: {
+                    "Content-Type": "application/json",
+                    ...headers
+                },
+                data: data ? JSON.stringify(data) : null,
+
+                onload: (res) => {
+                    log(method, url, "->", res.status);
+
+                    try {
+                        resolve(JSON.parse(res.responseText));
+                    } catch (e) {
+                        resolve(res.responseText);
+                    }
+                },
+
+                onerror: (err) => {
+                    MITOS.log.error("Azure API request error: " + err);
+                    reject(err);
+                }
+            });
+        });
+    }
+
+    function buildAuthHeader(pat) {
+        if (!pat) throw new Error("Azure PAT is missing");
+
+        return {
+            Authorization: "Basic " + btoa(":" + pat)
+        };
+    }
+
+    window.MITOS.azdo.get = function (url, pat) {
+        return apiRequest({ method: "GET", url, headers: buildAuthHeader(pat) });
+    };
+
+    window.MITOS.azdo.api.post = function (url, data, pat) {
+        return apiRequest({ method: "POST", url, data, headers: buildAuthHeader(pat) });
+    };
+})();
