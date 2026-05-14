c2101aa2c11911b8c1f64b7c1538dd97d9036bf5
agrigorakis-tech <agrigorakis@programize.com>
2026-05-14 14:49:58 +0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,63 @@
+(function mitosAPI() {
+    'use strict';
+
+    window.MITOS = window.MITOS || {};
+    MITOS.api = MITOS.api || {};
+
+    function apiRequest({ method = "GET", url, data = null }) {
+        return new Promise((resolve, reject) => {
+
+            //MITOS.log.info("MITOS API request [" + method + "] | " + url);
+            try {
+                // Fetch Authorization token
+                const token = localStorage.getItem('ls.token')?.replace(/^"|"$/g, '');
+
+                if (!token) {
+                    MITOS.log.error("Could not fetch authorization token");
+                    return reject(new Error("Could not fetch authorization token"));
+                }
+
+                GM_xmlhttpRequest({
+                    method,
+                    url,
+                    headers: {
+                        "Authorization": "Bearer " + token, 
+                        "Content-Type": "application/json"
+                    },
+                    // Payload
+                    data: data ? JSON.stringify(data) : null,
+                    // Request
+                    onload: res => {
+                        MITOS.log.info("MITOS API [" + method + "] <" + url + "> | Completed " + res.status);
+                        try {
+                            resolve(JSON.parse(res.responseText));
+                        } 
+                        catch (err) {
+                            MITOS.log.warn("MITOS API [" + method + "] <" + url + "> | Could not parse response data: " + err);
+                            reject(err);
+                        }
+                    },
+                    onerror: err => {
+                        MITOS.log.error("MITOS API [" + method + "] <" + url + "> | Failed: " + err);
+                        reject(err);
+                    }
+                });
+
+            } 
+            catch (err) {
+                MITOS.log.error("MITOS API request error:" + err);
+                reject(err);
+            }
+        });
+    }
+
+    MITOS.api.get = function mitosAPI_GET(url) {
+
+        return apiRequest({ method: "GET", url });
+    }
+
+    MITOS.api.post = function mitosAPI_POST(url) {
+        
+        return apiRequest({ method: "POST", url, data });
+    }
+})();
